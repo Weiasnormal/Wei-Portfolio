984a48a94f522fb1467033761fa91989cddb7ab5
Weiasnormal <crusitwincel@gmail.com>
2026-07-22 22:21:37 +0800

    remvoe the app destination

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/app',
-        permanent: true,
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
